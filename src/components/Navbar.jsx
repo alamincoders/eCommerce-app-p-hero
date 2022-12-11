4edb8fabd10856f0b68195ffb0d725ec06dc73c7
alamincoders <developeralamin03@gmail.com>
2022-12-11 18:54:39 +0600

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
           </div>
           <input type="radio" name="slide" id="menu_btn" />
           <input type="radio" name="slide" id="cancel_btn" />
+
           <ul class="nav_links">
             <label for="cancel_btn" class="btn cancel_btn">
               <i class="fas fa-times"></i>
@@ -24,46 +25,17 @@ const Navbar = () => {
             </li>
             <li>
               <a href="#" class="desktop_item">
-                Dropdown Menu
-              </a>
-              <input type="checkbox" id="showDrop" />
-              <label for="showDrop" class="mobile_item">
-                Dropdown Menu
-              </label>
-              <ul class="drop_menu">
-                <li>
-                  <a href="#">Drop Menu 1</a>
-                </li>
-                <li>
-                  <a href="#">Drop Menu 2</a>
-                </li>
-                <li>
-                  <a href="#">Drop Menu 3</a>
-                </li>
-                <li>
-                  <a href="#">Drop Menu 4</a>
-                </li>
-                <li>
-                  <a href="#">Drop Menu 5</a>
-                </li>
-                <li>
-                  <a href="#">Drop Menu 6</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#" class="desktop_item">
-                Mega Menu
+                Products
               </a>
               <input type="checkbox" id="showMega" />
               <label for="showMega" class="mobile_item">
-                Mega Menu
+                Products
               </label>
               <div class="mega_box">
                 <div class="content">
                   {/* <!--  --> */}
                   <div class="row img_row">
-                    <header>Our Services</header>
+                    <header>Category</header>
                     <div class="services_img"></div>
                   </div>
                   {/* <!--  --> */}
@@ -126,10 +98,45 @@ const Navbar = () => {
                 </div>
               </div>
             </li>
+            {/*  */}
+            <li>
+              <a href="#" class="desktop_item">
+                Explore
+              </a>
+              <input type="checkbox" id="showDrop" />
+              <label for="showDrop" class="mobile_item">
+                Explore
+              </label>
+              <ul class="drop_menu">
+                <li>
+                  <a href="#">Drop Menu 1</a>
+                </li>
+                <li>
+                  <a href="#">Drop Menu 2</a>
+                </li>
+                <li>
+                  <a href="#">Drop Menu 3</a>
+                </li>
+                <li>
+                  <a href="#">Drop Menu 4</a>
+                </li>
+                <li>
+                  <a href="#">Drop Menu 5</a>
+                </li>
+                <li>
+                  <a href="#">Drop Menu 6</a>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <a href="#">Customer Care</a>
+            </li>
             <li>
               <a href="#">Feedback</a>
             </li>
           </ul>
+
           <label for="menu_btn" class="btn menu_btn">
             <i class="fas fa-bars"></i>
           </label>
