8e1ed062480cd1b0e2e3c56d68b52a2d50063a54
alamincoders <developeralamin03@gmail.com>
2022-12-11 19:22:13 +0600

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import logo from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
@@ -8,7 +9,9 @@ const Navbar = () => {
       <nav>
         <div class="wrapper">
           <div class="logo">
-            <a href="#">Logo</a>
+            <a href="#">
+              <img src={logo} alt="" />
+            </a>
           </div>
           <input type="radio" name="slide" id="menu_btn" />
           <input type="radio" name="slide" id="cancel_btn" />
@@ -36,7 +39,13 @@ const Navbar = () => {
                   {/* <!--  --> */}
                   <div class="row img_row">
                     <header>Category</header>
-                    <div class="services_img"></div>
+                    <div class="w-full rounded h-[200px]">
+                      <img
+                        src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/01/category-pages-featured-image-5ffbf8cca689f.png"
+                        alt=""
+                        className="w-full h-full object-fill object-center rounded drop-shadow-lg"
+                      />
+                    </div>
                   </div>
                   {/* <!--  --> */}
                   <div class="row">
@@ -128,12 +137,33 @@ const Navbar = () => {
                 </li>
               </ul>
             </li>
-
             <li>
               <a href="#">Customer Care</a>
             </li>
-            <li>
-              <a href="#">Feedback</a>
+            <li title="cart" className="ml-5 relative">
+              <a href="#" className="lg:absolute top-[11px] left-0 ">
+                <div class="w-3.5 h-3.5 flex items-center justify-center bg-[rgb(14,165,233)] absolute top-1.5 right-1.5 rounded-full text-[10px] leading-none text-white font-medium">
+                  <span class="mt-[1px]">3</span>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                  />
+                </svg>
+              </a>
+            </li>{" "}
+            <li title="login/register" className="relative">
+              <a href="#" className="lg:absolute top-[11px] left-10 ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
+              </a>
             </li>
           </ul>
 
