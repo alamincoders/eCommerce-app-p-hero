0ba0c023a23f7d207c44db9ff86195c0bba372f6
alamincoders <developeralamin03@gmail.com>
2022-12-12 18:58:29 +0600

    updated

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -51,14 +51,14 @@ const Footer = () => {
                   className="bg-white focus:outline-none focus:ring-2 px-5 w-72 py-3  shadow-red-100 shadow-2xl rounded"
                   placeholder="Enter your email"
                 />
-                <button type="submit" class="relative inline-block text-lg group mt-5 lg:mt-0">
-                  <span class="relative z-10 block px-5 py-2.5 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                    <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                    <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-                    <span class="relative">Subscribe</span>
+                <button type="submit" className="relative inline-block text-lg group mt-5 lg:mt-0">
+                  <span className="relative z-10 block px-5 py-2.5 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                    <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                    <span className="relative">Subscribe</span>
                   </span>
                   <span
-                    class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
                     data-rounded="rounded-lg"
                   ></span>
                 </button>
